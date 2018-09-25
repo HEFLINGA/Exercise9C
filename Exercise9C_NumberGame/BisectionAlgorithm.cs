28011bca15b5f4559b434a7d5cfcbd334865b8ae
Auston Hefling <auston.hefling@outlook.com>
2018-09-25 16:04:32 -0400

    Finished

--- Exercise9C_NumberGame/BisectionAlgorithm.cs
+++ Exercise9C_NumberGame/BisectionAlgorithm.cs
@@ -8,33 +8,171 @@ namespace Exercise9C_NumberGame
 {
     class BisectionAlgorithm
     {
-        int[] list = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        int input = 0;
+        int iterations = 0;
+        int total = 0;
+        int count = 0;
 
-        private void Bisection(int input, int[] list)
+        private int BisectionAlg(int[] list, int input)
         {
-            int tmp = 0;
-            Console.WriteLine("Input a number between 1 and 10");
-            input = int.Parse(Console.ReadLine());
+            int left = 0;
+            int right = list.Length;            
 
-            if (input >= 1 && input <= 10)
+            while (left <= right)
             {
-                for(int i = 0; i < list.Length; i++)
+                int center = left + (right - left) / 2;
+                iterations++;
+                Console.WriteLine($"{center}");
+
+                if (list[center] > input)
                 {
-                    tmp = list[i];
-                    Console.WriteLine($"{tmp}");                    
+                    right = center - 1;
                 }
-                Console.WriteLine($"Your number is {tmp}");
+                else if (list[center] < input)
+                {
+                    left = center + 1;
+                }
+                else
+                {
+                    return center;
+                }
             }
-            else
+
+            return iterations;
+        }
+        private void HumPlaysGuesser(int compInput)
+        {
+            iterations = 0;
+            double avg = 0.0;
+            bool guessedIt = false;
+            int input = 0;
+            Console.WriteLine("Please guess a number between 1 and 1000.. human..");
+
+            while (guessedIt == false)
             {
-                Console.WriteLine("No valid input detected. Program termination imminent");
-            }
+                input = int.Parse(Console.ReadLine());
+                if (input > 0 || input <= 1000)
+                {
+                    if (input > compInput)
+                    {
+                        Console.WriteLine("To High. Guess again.");
+                        iterations++;
+                    }
+                    else if (input < compInput)
+                    {
+                        Console.WriteLine("To Low. Guess again.");
+                        iterations++;
+                    }
+                    else if (input == compInput)
+                    {
+                        total += iterations;
+                        count += 1;
+                        avg = total / count;
+                        Console.WriteLine($"You guessed it, number was {compInput} alllll alonnnngggg, and took you {iterations} iterations." +
+                            $"\nTaking you an avarage (so far) of {avg}");
+                        
+                        guessedIt = true;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No valid input");
+                }                
+            }            
+        }
+        private void CompPlaysGuesser()
+        {
+            iterations = 0;
+            int[] list = new int[100];
+            int input = 0;
+
+            bool cont = true;
+            int left = 0;
+            int right = list.Length;            
+
+            while (left <= right && cont == true)
+            {
+                int center = left + (right - left) / 2;
+                iterations++;
+                Console.WriteLine("1. for to high \n2. for to low. \n3. for JUUSTT RIGHT!");
+                Console.WriteLine($"Guess: {center}");
+                input = int.Parse(Console.ReadLine());
+
+                if (input == 1)
+                {
+                    right = center - 1;                    
+                }
+                else if (input == 2)
+                {
+                    left = center + 1;
+                }
+                else if (input == 3)
+                {
+                    total += iterations;
+                    count += 1;
+                    double avg = total / count;
+                    Console.WriteLine($"Computer got it right, taking {iterations},  average is: {avg}");
+                    cont = false;
+                }
+            }           
         }
 
-        public void Run()
+        public void RunBasic()
         {
-            Bisection(input, list);
+            int[] list = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int input = 0;
+            Console.WriteLine("Please enter your number Between 1 and 10");
+            input = int.Parse(Console.ReadLine());
+
+            if (input <= 0 || input > 10)
+            {
+                Console.WriteLine("No valid input");
+            }
+            else if(input > 0 && input <= 10)
+            {
+                Console.WriteLine($"Your number was {BisectionAlg(list, input)}, it took {iterations} iterations."); 
+            }            
+        }
+        public void RunHumanPlays()
+        {
+            string answer = "";
+            bool again = true;
+            do
+            {
+                Random rnd = new Random();
+                int compInput = rnd.Next(1, 1000);
+                HumPlaysGuesser(compInput);
+                Console.WriteLine("Go again? Y for yes, N for no.");
+                answer = Console.ReadLine().ToLower();
+                if (answer == "y")
+                {
+                    again = true;
+                }
+                else if (answer == "n")
+                {
+                    again = false;
+                }
+            } while (again == true);
+            
+        }
+        public void RunCompPlays()
+        {
+            string answer = "";
+            bool again = true;
+            do
+            {
+                CompPlaysGuesser();
+                Console.WriteLine("Go again? Y for yes, N for no.");
+                answer = Console.ReadLine().ToLower();
+                if (answer == "y")
+                {
+                    again = true;
+                }
+                else if (answer == "n")
+                {
+                    again = false;
+                }
+            } while (again == true);
+
         }
     }
 }
--- Exercise9C_NumberGame/Program.cs
+++ Exercise9C_NumberGame/Program.cs
@@ -10,9 +10,27 @@ namespace Exercise9C_NumberGame
     {
         static void Main(string[] args)
         {
+            int input;
             BisectionAlgorithm b = new BisectionAlgorithm();
 
-            b.Run();
+            Console.WriteLine("Basic Bisection Algorithm: 1. " +
+                "\n\nGuess my number, human plays: 2." +
+                "\n\nGuess my number, computer plays: 3.");
+            input = int.Parse(Console.ReadLine());
+
+            switch (input)
+            {
+                case 1:
+                    b.RunBasic();
+                    break;
+                case 2:
+                    b.RunHumanPlays();
+                    break;
+                case 3:
+                    b.RunCompPlays();
+                    break;
+            }
+            
         }
     }
 }
